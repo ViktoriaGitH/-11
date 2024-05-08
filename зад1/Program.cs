c8009e750859e84c913a9b44d4cbe513218a25ed
Viktoria Hristova <Viktoria Hristova@DESKTOP-7D09794>
2024-05-08 13:05:24 +0300

    Add project files.

---
+++ зад1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace зад1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet[] pets = new Pet[]
+        {
+            new Dog { Name = "Али", Age = 3 },
+            new Dog { Name = "Макс", Age = 5 },
+            new Cat { Name = "Луси", Age = 2 },
+            new Cat { Name = "Пухи", Age = 4 },
+            new Dog { Name = "Шаро", Age = 2 }
+        };
+
+            foreach (var pet in pets)
+            {
+                Console.WriteLine(pet.Info());
+                Console.WriteLine($"Звук: {pet.GetSound()}");
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+    class Pet
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual string GetSound()
+        {
+            return "Unknown";
+        }
+
+        public virtual string Info()
+        {
+            return $"Име: {Name}, Години: {Age}";
+        }
+    }
+
+    class Cat : Pet
+    {
+        public override string GetSound()
+        {
+            return "Мяу";
+        }
+
+        public override string Info()
+        {
+            return $"Котка - {base.Info()}";
+        }
+    }
+
+    class Dog : Pet
+    {
+        public override string GetSound()
+        {
+            return "Бау";
+        }
+
+        public override string Info()
+        {
+            return $"Куче - {base.Info()}";
+        }
+    }
+
+}
---
+++ зад2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace зад2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dot2D dot2D = new Dot2D(1, 2);
+            Dot3D dot3D = new Dot3D(3, 4, 5);
+
+            dot2D.PrintCoordinates();
+            dot3D.PrintCoordinates();
+        }
+    }
+
+    class Dot2D
+    {
+        protected double x;
+        protected double y;
+
+        public Dot2D()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Dot2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public virtual void PrintCoordinates()
+        {
+            Console.WriteLine($"2D точка: ({x}, {y})");
+        }
+    }
+
+    class Dot3D : Dot2D
+    {
+        protected double z;
+
+        public Dot3D()
+        {
+            z = 0;
+        }
+
+        public Dot3D(double x, double y, double z) : base(x, y)
+        {
+            this.z = z;
+        }
+
+        public override void PrintCoordinates()
+        {
+            Console.WriteLine($"3D точка: ({x}, {y}, {z})");
+        }
+    }
+
+}
